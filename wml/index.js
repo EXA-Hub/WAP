255501a3ac4d76054d79fc048b67e24fa5cadd3d
EXA-Hub <ragabadny4@gmail.com>
2023-12-03 15:12:14 +0200

    everythingis working fine (UNEXPECTEDLY)

--- wml/index.js
+++ wml/index.js
@@ -6,9 +6,10 @@ import fs from 'fs';
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const chatFilePath = path.join(__dirname, 'chat.json');
+const chatWmlFilePath = path.join(__dirname, 'chat.wml'); // Path for the new chat.wml file
 
 app.use(express.static('images'));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // To parse URL-encoded body
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -18,49 +19,57 @@ app.get("/", (req, res) => {
 const wmlFilePath = path.join(__dirname, 'http.wml');
 const wmlPageTemplate = fs.readFileSync(wmlFilePath, 'utf8');
 
-app.get("/http.wml", (req, res) => {
-    res.type("text/vnd.wap.wml");
-    res.sendFile("http.wml", { root: __dirname });
-});
-
 // Function to save chat messages to JSON file
 function saveChatMessage(message) {
-    fs.readFile(chatFilePath, (err, data) => {
-        if (err && err.code === 'ENOENT') {
-            // If the file doesn't exist, create it with the first message
-            return fs.writeFile(chatFilePath, JSON.stringify([message]), (err) => {
-                if (err) throw err;
-            });
-        } else if (err) {
-            throw err;
-        }
+    let messages = [];
+    // Check if the chat file exists and read its content
+    if (fs.existsSync(chatFilePath)) {
+        messages = JSON.parse(fs.readFileSync(chatFilePath, 'utf8'));
+    }
+    // Append the new message
+    messages.push(message);
+    // Write the updated messages back to the file
+    fs.writeFileSync(chatFilePath, JSON.stringify(messages, 1, 1), 'utf8');
+}
 
-        // If the file exists, append the new message
-        const messages = JSON.parse(data);
-        messages.push(message);
-        fs.writeFile(chatFilePath, JSON.stringify(messages), (err) => {
-            if (err) throw err;
-        });
-    });
+function saveChatWML(req) {
+    // Check for query parameters to save a new message
+    if (req.query.author && req.query.text) {
+        const message = { author: req.query.author, text: req.query.text };
+        saveChatMessage(message);
+    }
+
+    // Read the chat messages and format them for WML display
+    let formattedMessages = 'No messages yet.';
+    if (fs.existsSync(chatFilePath)) {
+        const messages = JSON.parse(fs.readFileSync(chatFilePath, 'utf8'));
+        formattedMessages = messages.map(msg => `${msg.author}: ${msg.text}`).join('\n      ');
+    }
+
+    // Inject the formatted messages into the WML page
+    const wmlPageWithMessages = wmlPageTemplate.replace('$(chatMessages)', formattedMessages);
+
+    // Save the updated content to the new chat.wml file
+    fs.writeFileSync(chatWmlFilePath, wmlPageWithMessages, 'utf8');
 }
 
 app.get('/chat.wml', (req, res) => {
-    fs.readFile(chatFilePath, (err, data) => {
-        if (err && err.code === 'ENOENT') {
-            // If the file doesn't exist, send an empty chat page
-            return res.type('text/vnd.wap.wml').send(wmlPageTemplate.replace('$(chatMessages)', 'No messages yet.'));
-        } else if (err) {
-            return res.status(500).send('Error reading chat file');
+    // Set the content type and send the chat.wml file as a response
+    saveChatWML(req); // Execute the function after the file is sent
+    res.type("text/vnd.wap.wml");
+    res.sendFile("chat.wml", { root: __dirname }, function (err) {
+        if (err) {
+            console.log("Error sending file:", err);
+        } else {
+            console.log("File sent successfully.");
         }
-
-        // Format the chat messages for WML display
-        const messages = JSON.parse(data);
-        const formattedMessages = messages.map(msg => `<p>${msg.author}: ${msg.text}</p>`).join('');
-
-        // Inject the formatted messages into the WML page
-        const wmlPageWithMessages = wmlPageTemplate.replace('$(chatMessages)', formattedMessages);
-        res.type('text/vnd.wap.wml').send(wmlPageWithMessages);
     });
+});
+
+
+app.get("/http.wml", (req, res) => {
+    res.type("text/vnd.wap.wml");
+    res.sendFile("http.wml", { root: __dirname });
 });
 
 app.listen(3000, () => {
